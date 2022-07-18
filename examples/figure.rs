651e4b3f28292273d1646c9988523f7abea630be
Ivan L <tsionyx@gmail.com>
2022-07-18 16:12:28 +0400

    add README with example of usage

---
+++ examples/figure.rs
@@ -0,0 +1,69 @@
+use pg_helper::{array_type, struct_type, Column, ColumnBuilder, PgTableExtension, Table};
+use postgres::{
+    types::{FromSql, ToSql, Type},
+    Client, Error, Row,
+};
+
+#[derive(Debug, Copy, Clone, PartialEq, ToSql, FromSql)]
+#[postgres(name = "point2d")]
+struct Point {
+    x: i16,
+    y: i16,
+}
+
+#[derive(Debug, PartialEq)]
+struct Figure {
+    name: String,
+    polygon: Vec<Point>,
+}
+
+impl Table<2> for Figure {
+    fn name() -> &'static str {
+        "figures"
+    }
+
+    fn columns() -> [Column; 2] {
+        let point_type = struct_type("point2d", &[("x", Type::INT2), ("y", Type::INT2)]);
+        [
+            ColumnBuilder::new("name", Type::VARCHAR).index().finish(),
+            Column::new("polygon", array_type(point_type)),
+        ]
+    }
+
+    fn values(&self) -> [&(dyn ToSql + Sync); 2] {
+        [&self.name, &self.polygon]
+    }
+}
+
+impl TryFrom<Row> for Figure {
+    type Error = Error;
+
+    fn try_from(value: Row) -> Result<Self, Self::Error> {
+        let name = value.try_get("name")?;
+        let polygon = value.try_get("polygon")?;
+        Ok(Self { name, polygon })
+    }
+}
+
+fn main() {
+    let db_url = std::env::var("DATABASE_URL").unwrap();
+    let mut client = Client::connect(&db_url, postgres::NoTls).unwrap();
+
+    client.create_table::<Figure, 2>().unwrap();
+
+    let fig = Figure {
+        name: "trapezoid".into(),
+        polygon: vec![
+            Point { x: 0, y: 0 },
+            Point { x: 2, y: 4 },
+            Point { x: 3, y: 4 },
+            Point { x: 6, y: 0 },
+        ],
+    };
+    client.insert_row(&fig).unwrap();
+    println!("Figure inserted!");
+
+    let items: Vec<Figure> = client.select_all().unwrap();
+    println!("{} figures selected!", items.len());
+    assert_eq!(items[0], fig);
+}
